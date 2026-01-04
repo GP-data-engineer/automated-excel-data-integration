f5d26e34a6409a7260ff5429cde99b621ac61578
Grzegorz.P <60967996+GP-data-engineer@users.noreply.github.com>
2026-01-04 12:52:10 +0100

    Update db_utils.py

--- src/db_utils.py
+++ src/db_utils.py
@@ -4,4 +4,4 @@ import pandas as pd
 def save_to_db(df, table_name, connection_string):
     engine = create_engine(connection_string)
     df.to_sql(table_name, engine, if_exists="replace", index=False)
-    print(f"✅ Saved {len(df)} rows into {table_name}")
+    print(f"Saved {len(df)} rows into {table_name}")
